9fa87f9a061c0d7ae7d30a01beafdebb2de948bb
Natcha119 <natcha119@hotmail.com>
2013-10-02 15:48:02 +0700

    Create InventoryController

--- AndroidPOS/src/com/example/androidpos/Inventory.java
+++ AndroidPOS/src/com/example/androidpos/Inventory.java
@@ -1,25 +1,26 @@
 package com.example.androidpos;
+
 import android.R.bool;
 
 public class Inventory {
-	
+
 	public boolean create() {
-		
+
 	}
-	
+
 	public boolean edit() {
-		
+
 	}
-	
+
 	public boolean remove() {
-		
+
 	}
-	
+
 	public boolean update() {
-		
+
 	}
-	
+
 	public String search() {
-		
+
 	}
 }
