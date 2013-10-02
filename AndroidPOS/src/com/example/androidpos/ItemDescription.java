c5d03379d219f40b99c4436b57c967905910e91e
zee <zeemyself@gmail.com>
2013-10-02 15:02:45 +0700

    Add Inventory description by zee

---
+++ AndroidPOS/src/com/example/androidpos/ItemDescription.java
@@ -0,0 +1,26 @@
+package com.example.androidpos;
+
+public class ItemDescription {
+	public ItemDescription(String name, int ID, int quantity,
+			String description, String unit, double price, double cost) {
+		this.name = name;
+		this.ID = ID;
+		this.quantity = quantity;
+		this.description = description;
+		this.unit = unit;
+		this.price = price;
+		this.cost = cost;
+	}
+
+	private String name;
+	private int ID;
+	private int quantity;
+	private String description;
+	private String unit;
+	private double price;
+	private double cost;
+
+	public String getDiscription() {
+		return description;
+	}
+}
