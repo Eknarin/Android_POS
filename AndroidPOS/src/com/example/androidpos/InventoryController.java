23c4ca6d552306f6a7bb550d7db5d148cc45e27c
Natcha119 <natcha119@hotmail.com>
2013-10-02 16:31:24 +0700

    create InventoryController

---
+++ AndroidPOS/src/com/example/androidpos/InventoryController.java
@@ -0,0 +1,26 @@
+package com.example.androidpos;
+
+public class InventoryController {
+	private Inventory invent;
+
+	public void addItem(String id, String name) {
+
+	}
+
+	public void editItem() {
+
+	}
+
+	public void removeItem() {
+
+	}
+
+	public void searchItem() {
+
+	}
+
+	public String[] getItemDescription() {
+		return null;
+
+	}
+}
