d53a903bf71c625383e68db362fdaf8dac2f71ad
Eknarin <oat-et@hotmail.com>
2013-10-02 15:34:24 +0700

    initialize Inventory

---
+++ AndroidPOS/src/com/example/androidpos/Inventory.java
@@ -0,0 +1,25 @@
+package com.example.androidpos;
+import android.R.bool;
+
+public class Inventory {
+	
+	public boolean create() {
+		
+	}
+	
+	public boolean edit() {
+		
+	}
+	
+	public boolean remove() {
+		
+	}
+	
+	public boolean update() {
+		
+	}
+	
+	public String search() {
+		
+	}
+}
